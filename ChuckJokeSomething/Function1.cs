a93de51f41cc39a4ca32130278c166b6ab9d8807
robin <robin.kamo@iths.se>
2022-11-18 13:42:49 +0100

    Add project files.

---
+++ ChuckJokeSomething/Function1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Twilio.TwiML;
+
+namespace ChuckJokeSomething
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var joke = await JokeHelper.GetJokeAsync();
+            var response = new MessagingResponse().Message(joke);
+            var twiml = response.ToString();
+
+            return new ContentResult
+            {
+                Content = twiml,
+                ContentType = "application/xml"
+            };
+
+
+
+        }
+    }
+}
---
+++ ChuckJokeSomething/JokeHelper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckJokeSomething
+{
+    class JokeHelper
+    {
+        
+        public static async Task<String> GetJokeAsync()
+        {
+            var joke = "No joke for you";
+
+            using (var httpClient = new HttpClient())
+            {
+            
+                var apiEndPoint = "https://api.chucknorris.io/jokes/random";
+                httpClient.BaseAddress = new Uri(apiEndPoint);
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var responseMessage = await httpClient.GetAsync(apiEndPoint);
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var jsonContent = await responseMessage.Content.ReadAsStringAsync();
+                    dynamic data = JsonConvert.DeserializeObject(jsonContent);
+                    joke = data.value;
+                }
+
+            }
+            return joke;
+        }
+
+    }
+}
